8f31e5cdd5ba2262412cc361c2a8f84d2aecf674
José Sérgio <sergiosilva@outlook.com>
2022-08-28 11:55:41 -0300

    Finished mini project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :email, uniqueness: true
+  validates :username, length: { in: 3..12 }, presence: true, uniqueness: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, uniqueness: true
-  validates :username, uniqueness: true
+  def username
+    email.split('@')[0].capitalize
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :posts
   devise_scope :user do
     # Redirests signing out users back to sign-in
-    get "users", to: "devise/sessions#new"
+    get 'users', to: 'devise/sessions#new'
   end
   devise_for :users
 
-  root to: "posts#index"
+  root to: 'posts#index'
 end
